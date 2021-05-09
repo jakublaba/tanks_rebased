eeef3dc4dfce23c32463b840fc212938f6110afc
DanielSlusarczyk <daniel00150015@gmail.com>
2021-05-09 21:37:38 +0200

    Dodanie podstawowego interfejsu i przycisków

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,13 +1,27 @@
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 import java.util.List;
 
 public final class Controller {
-    private List<Button> menuButtons;
+    //private List<Button> menuButtons;
 
-    private void startButtonPressed() {}
-    private void settingsButtonPressed() {}
-    private void exitButtonPressed() {}
+
+    @FXML
+    private void startButtonPressed() {
+        System.out.println("Hej");
+    }
+
+    @FXML
+    private void settingsButtonPressed() {
+        System.out.println("Tu będą ustawienia");
+    }
+
+    @FXML
+    private void exitButtonPressed() {
+        System.exit(1);
+    }
+
     private void pauseButtonPressed() {}
     private void loadConfigFile() {}
 }
