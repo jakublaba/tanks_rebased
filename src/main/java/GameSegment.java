b558421c1c71e1422b0ba8ddba41ac3e2a3909f3
jakublaba <jakub.maciej.laba@gmail.com>
2021-06-09 10:22:26 +0200

    pull

--- src/main/java/GameSegment.java
+++ src/main/java/GameSegment.java
@@ -1,6 +1,4 @@
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 
 public abstract class GameSegment {
     protected double x, y, currentSize, currentVelocity;
